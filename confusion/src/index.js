57d46029051b01cb9e53cd35b986aac8fcb04558
jbegson <71764526+jbegson@users.noreply.github.com>
2022-06-22 15:55:30 +0200

    Assignment 3: React Forms and Redux
    - Adding a new class component named CommentForm
    - Constructing the form for users to submit their comments
    - Enabling form validation

--- confusion/src/index.js
+++ confusion/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM  from 'react-dom';
+import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import 'font-awesome/css/font-awesome.min.css';
