43087e7d308a84190cf4085277f0671c5bc3146b
jbegson <71764526+jbegson@users.noreply.github.com>
2022-06-22 15:52:40 +0200

    Assignment 2: React Router and Single Page Applications
    - Integrating the AboutComponent into the single page application
    - Implementing a new functional component named <RenderLeader> through the RenderLeader() function
    - Using the <RenderLeader> component created in the previous task to render a list of leaders within your AboutComponent

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,84 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
-import { DISHES } from './shared/dishes';
+
+import Main from './components/MainComponent';
+
+import './App.css';
+
+import { BrowserRouter } from 'react-router-dom';
+
 
 
 class App extends Component{
 
-  constructor(props){
-    super(props);
-
-    this.state = {
-      dishes: DISHES
-    };
-
-  } 
 
   render(){
 
     return(
-      <div>
-        <Navbar dark color="primary">
-          <div className="container">
-              <NavbarBrand href="/"> Ristorante Con Fusion </NavbarBrand>
-          </div>
-        </Navbar>
-
-        <Menu dishes={ this.state.dishes } />
-      </div>
+      <BrowserRouter>
+        <div>
+          <Main />
+        </div>      
+      </BrowserRouter>
     );
 
   }
 
 }
 
-/*
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-      </div>
-    );
-  }
-}
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    };
-  }
-}
-<Menu dishes={this.state.dishes} />
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-*/
 export default App;
