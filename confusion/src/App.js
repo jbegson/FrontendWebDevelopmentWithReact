9a81ae07b34fe60eb92f5c36ce67f53d925f00fc
jbegson <71764526+jbegson@users.noreply.github.com>
2022-06-22 15:37:21 +0200

    Assignment 1: React Components
    - Adding a new DishdetailComponent and include the component into the menu component's view
    - Adding a card component to the DishdetailComponent view to display the details of the dish
    - Use the comments that are included in the dish object above to display a list of the comments for the dish

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,5 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
+
+
+class App extends Component{
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+
+  } 
+
+  render(){
+
+    return(
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+              <NavbarBrand href="/"> Ristorante Con Fusion </NavbarBrand>
+          </div>
+        </Navbar>
+
+        <Menu dishes={ this.state.dishes } />
+      </div>
+    );
+
+  }
+
+}
+
+/*
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+}
+<Menu dishes={this.state.dishes} />
 
 function App() {
   return (
@@ -21,5 +80,5 @@ function App() {
     </div>
   );
 }
-
+*/
 export default App;
